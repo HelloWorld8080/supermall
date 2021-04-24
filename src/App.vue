a66384af6cfdf31adf6c0bd0ff2fc8987a5e4740
liuxun <1193127296@qq.com>
2021-04-24 18:46:53 +0800

    tabbar展示

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="nav">
+    <main-tab-bar></main-tab-bar>
+  </div>
+</template>
+<script>
+  import MainTabBar from "./components/content/MainTabBar";
+  export default {
+    name:"app",
+    components:{
+      MainTabBar
+    }
+  }
+</script>
+<style>
+
+</style>
---
+++ src/components/content/MainTabBar.vue
@@ -0,0 +1,38 @@
+﻿<template>
+   <div id="main-tab-bar">
+     <TabBar>
+       <TabBarItem>
+         <img src="~assets/img/tabbar/home.svg" slot="item-icon">
+         <div slot="item-text">首页</div>
+       </TabBarItem>
+       <TabBarItem>
+         <img src="~assets/img/tabbar/category.svg" slot="item-icon">
+         <div slot="item-text">分类</div>
+       </TabBarItem>
+       <TabBarItem>
+         <img src="~assets/img/tabbar/shopcart.svg" slot="item-icon">
+         <div slot="item-text">购物车</div>
+       </TabBarItem>
+       <TabBarItem>
+         <img src="~assets/img/tabbar/profile.svg" slot="item-icon">
+         <div slot="item-text">我的</div>
+       </TabBarItem>
+     </TabBar>
+   </div>
+
+</template>
+<script>
+import TabBar from "components/common/TabBar";
+import TabBarItem from "components/common/TabBarItem";
+export default {
+  name: "MainTabBar",
+  components:{
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
